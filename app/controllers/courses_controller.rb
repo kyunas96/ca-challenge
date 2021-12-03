f08841eb422b4d266cdf311d73269799ecc5d8c4
Kevin Yunas <kevinyunas@icloud.com>
2021-12-02 19:33:15 -0800

    Dealt with Date and Times in CoursesController#create

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,21 +8,67 @@ class CoursesController < ApplicationController
   def show
     course = Course.find_by(id: course_params[:id])
 
-    if(course)
+    if course
       render json: course
+      return
     else
-      render json: {error: "exist"}
+      render json: {error: "Course does not exist"}
+      return
     end
 
   end
 
+=begin
+  course_params: {
+    name: string,
+    release_date: {
+      month: int,
+      day: int,
+      hour: int,
+      minute: int
+    }
+  }
+=end
+
   def create
-    course = 
+    # course_params[:release_date] must be a hash that has values for month,
+    # day, year and time
+
+    formatted_release_date = DateTime.new(
+      course_params[:release_date][:year],
+      course_params[:release_date][:month],
+      course_params[:release_date][:day],
+      course_params[:release_date][:hour],
+      course_params[:release_date][:minute],
+    )    
+
+    course = Course.new(
+      name: course_params[:name], 
+      release_date: formatted_release_date,
+      created_at: Time.now,
+      update_at: Time.now
+    )
+
+    if course.save
+      render json: course
+    else
+      render json: {error: "Unable to save course"}
+    end
+
+
+    
 
   end
 
   def update
+    course = Course.find_by(id: course_params[:id])
 
+    if(course)
+      
+
+    else
+      render json: {error: "Course does not exist"}
+    end
   end
 
   def destroy
