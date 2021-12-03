dd53912e0ecc4d00bb5813acfea3c98721916352
Kevin Yunas <kevinyunas@icloud.com>
2021-12-02 19:05:47 -0800

    Created CoursesController#index and #show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,10 +6,18 @@ class CoursesController < ApplicationController
   end
 
   def show
+    course = Course.find_by(id: course_params[:id])
+
+    if(course)
+      render json: course
+    else
+      render json: {error: "exist"}
+    end
 
   end
 
   def create
+    course = 
 
   end
 
@@ -19,6 +27,10 @@ class CoursesController < ApplicationController
 
   def destroy
 
+  end
+
+  def course_params
+    params.permit(:id, :name, :release_date)
   end
 
 end
