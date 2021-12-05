fe7d6901ec2625ecf0d5c79ef89d50307cbb44b9
Kevin Yunas <kevinyunas@icloud.com>
2021-12-05 12:46:43 -0800

    Completed CoursesController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -18,65 +18,106 @@ class CoursesController < ApplicationController
 
   end
 
-=begin
-  course_params: {
-    name: string,
-    release_date: {
-      month: int,
-      day: int,
-      hour: int,
-      minute: int
-    }
-  }
-=end
-
   def create
+    # on the frontend use a <input type="date" /> to ensure that the values passed
+    # to the backend are valid dates
+
     # course_params[:release_date] must be a hash that has values for month,
     # day, year and time
+    p "course params: " + course_params[:release_date].inspect
 
     formatted_release_date = DateTime.new(
-      course_params[:release_date][:year],
-      course_params[:release_date][:month],
-      course_params[:release_date][:day],
-      course_params[:release_date][:hour],
-      course_params[:release_date][:minute],
+      course_params[:release_date][:year].to_i,
+      course_params[:release_date][:month].to_i,
+      course_params[:release_date][:day].to_i,
+      course_params[:release_date][:hour].to_i,
+      course_params[:release_date][:minute].to_i,
     )    
 
     course = Course.new(
       name: course_params[:name], 
       release_date: formatted_release_date,
-      created_at: Time.now,
-      update_at: Time.now
+      created_at: DateTime.now,
+      updated_at: DateTime.now
     )
 
     if course.save
       render json: course
+      return
     else
       render json: {error: "Unable to save course"}
+      return
     end
 
-
-    
-
+    # render json: {error: "Unable to save course"}
   end
 
   def update
     course = Course.find_by(id: course_params[:id])
+    new_release_date = nil
+
+    if course_params[:release_date]
+        new_release_date = DateTime.new(
+          course_params[:release_date][:year].to_i,
+          course_params[:release_date][:month].to_i,
+          course_params[:release_date][:day].to_i,
+          course_params[:release_date][:hour].to_i,
+          course_params[:release_date][:minute].to_i
+        )
+    end
 
     if(course)
-      
+      p "Updating"
+      course.name = course_params[:name] ? course_params[:name] : course.name
+      course.release_date = new_release_date ? new_release_date : course.release_date
+      course.updated_at = DateTime.now
 
+      if course.save
+        render json: course, status: 200
+        return
+      else
+        render json: {error: ["Unable to update course"]}
+        return
+      end
     else
       render json: {error: "Course does not exist"}
     end
   end
 
   def destroy
+    course = Course.find_by(id: course_params[:id])
+
+    # Were users and auth to be implemented, logic would be implemented to check
+    # if the current user was an administrator with delete permissions
+
+    if(course)
+      course.destroy
+      render json: {message: "Course successfully deleted"}, status: 200
+    else
+      render json: {error: "Course does not exist"}, status: 422
+    end
 
   end
 
+
+=begin
+  course_params: {
+    name: string,
+    release_date: {
+      month: string,
+      day: string,
+      hour: string,
+      minute: string
+    }
+  }
+=end
+
   def course_params
-    params.permit(:id, :name, :release_date)
+    params.permit(
+      :id, 
+      :name, 
+      :release_date => [:year, :month, :day, :hour, :minute]\
+    )
   end
 
 end
